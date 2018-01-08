d9e0f3d3a71d3eb2b5b913dd0bc45ce9c41a9992
lijianan <lijianan@hujiang.com>
2018-01-08 15:17:16 +0800

    optimize spider code

--- anjuke/spiders/anjuke.py
+++ anjuke/spiders/anjuke.py
@@ -9,7 +9,7 @@ class AnjukeSpider(scrapy.Spider):
     page_index = 1
 
     start_urls = [
-        'https://shanghai.anjuke.com/sale/pudong',
+        'https://shanghai.anjuke.com/sale/putuo',
     ]
 
     def parse(self, response):
@@ -21,46 +21,46 @@ class AnjukeSpider(scrapy.Spider):
         info_list = response.xpath("//*[@id='houselist-mod-new']/li")
         for info in info_list:
             # 标题
-            title = info.xpath("./div[2]/div[1]/a/text()").extract()
+            title = info.xpath("./div[2]/div[1]/a/text()").extract_first()
             # 安选验真信息
-            guarantee_info = info.xpath("./div[2]/div[1]/em/@title").extract()
+            guarantee_info = info.xpath("./div[2]/div[1]/em/@title").extract_first()
             # 链接
-            link = info.xpath("./div[2]/div[1]/a/@href").extract()
+            link = info.xpath("./div[2]/div[1]/a/@href").extract_first()
             # 户型
-            house_type = info.xpath("./div[2]/div[2]/span[1]/text()").extract()
+            house_type = info.xpath("./div[2]/div[2]/span[1]/text()").extract_first()
             # 面积
-            area = info.xpath("./div[2]/div[2]/span[2]/text()").extract()
+            area = info.xpath("./div[2]/div[2]/span[2]/text()").extract_first()
             # 楼层信息
-            floor_info = info.xpath("./div[2]/div[2]/span[3]/text()").extract()
+            floor_info = info.xpath("./div[2]/div[2]/span[3]/text()").extract_first()
             # 建造时间
-            build_time_info = info.xpath("./div[2]/div[2]/span[4]/text()").extract()
+            build_time_info = info.xpath("./div[2]/div[2]/span[4]/text()").extract_first()
             # 经纪人姓名
-            broker_name = info.xpath("./div[2]/div[2]/span[5]/text()").extract()
+            broker_name = info.xpath("./div[2]/div[2]/span[5]/text()").extract_first()
             # 地址
-            address = info.xpath("./div[2]/div[3]/span/text()").extract()
+            address = info.xpath("./div[2]/div[3]/span/text()").extract_first()
             # 标签信息
             tags = []
             for tag in info.xpath("./div[2]/div[4]"):
                 tag_str = tag.xpath("./span/text()").extract()
-                tags.append(tag_str)
+                tags.extend(tag_str)
             # 价格
-            price = info.xpath("./div[3]/span[1]/strong/text()").extract()
+            price = info.xpath("./div[3]/span[1]/strong/text()").extract_first()
             # 每平米价格
-            unit_price = info.xpath("./div[3]/span[2]/text()").extract()
+            unit_price = info.xpath("./div[3]/span[2]/text()").extract_first()
 
             # 赋值到item对象上------------
-            item['title'] = title[0].strip() if title else ''
-            item['guarantee_info'] = guarantee_info[0] if guarantee_info else ''
-            item['link'] = link[0] if link else ''
-            item['house_type'] = house_type[0] if house_type else ''
-            item['area'] = area[0] if area else ''
-            item['floor_info'] = floor_info[0] if floor_info else ''
-            item['build_time_info'] = build_time_info[0] if build_time_info else ''
-            item['broker_name'] = broker_name[0] if broker_name else ''
-            item['address'] = address[0].strip() if address else ''
-            item['tags'] = tags[0] if tags else []
-            item['price'] = price[0] if price else ''
-            item['unit_price'] = unit_price[0] if unit_price else ''
+            item['title'] = title.strip() if title else ''
+            item['guarantee_info'] = guarantee_info if guarantee_info else ''
+            item['link'] = link if link else ''
+            item['house_type'] = house_type if house_type else ''
+            item['area'] = area if area else ''
+            item['floor_info'] = floor_info if floor_info else ''
+            item['build_time_info'] = build_time_info if build_time_info else ''
+            item['broker_name'] = broker_name if broker_name else ''
+            item['address'] = address.strip() if address else ''
+            item['tags'] = tags if tags else []
+            item['price'] = price if price else ''
+            item['unit_price'] = unit_price if unit_price else ''
             yield item
 
         # 下一页地址
