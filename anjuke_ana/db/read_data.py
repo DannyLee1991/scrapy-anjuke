5d21f7aa2ee05eda105c691d23da93f382fc01e5
李佳楠 <lijianan@hujiang.com>
2019-01-09 00:13:12 +0800

    add: 优化数据加载逻辑

--- anjuke_ana/db/read_data.py
+++ anjuke_ana/db/read_data.py
@@ -6,7 +6,7 @@ def get_all_locate_a():
     获取所有行政区
     :return:
     '''
-    r = read_sql("select distinct LOCATE_A from anjuke_data")
+    r = read_sql("select distinct LOCATE_A from anjuke_sale_data")
     locate_a_list = [item[0] for item in r.values]
     return locate_a_list
 
@@ -17,7 +17,7 @@ def get_all_locate_b(locate_a):
     :param locate_a:
     :return:
     '''
-    r = read_sql("select distinct LOCATE_B from anjuke_data where LOCATE_A = \"{locate_a}\"".format(locate_a=locate_a))
+    r = read_sql("select distinct LOCATE_B from anjuke_sale_data where LOCATE_A = \"{locate_a}\"".format(locate_a=locate_a))
     locate_b_list = [item[0] for item in r.values]
     return locate_b_list
 
@@ -28,7 +28,7 @@ def calc_average_price(locate_a):
                      count(*) as count,
                      sum(area) as total_area,
                      sum(price) as total_price
-                    from anjuke_data 
+                    from anjuke_sale_data 
                     where locate_a = '%s' 
                     group by locate_b """ % locate_a)
 
@@ -46,7 +46,7 @@ def calc_average_price(locate_a):
 if __name__ == '__main__':
     for locate_a in get_all_locate_a():
         print(locate_a)
-        # df = calc_average_price(locate_a)
-        # print(df)
-        for locate_b in get_all_locate_b(locate_a):
-            print("     " + locate_b)
+        df = calc_average_price(locate_a)
+        print(df)
+        # for locate_b in get_all_locate_b(locate_a):
+        #     print("     " + locate_b)
